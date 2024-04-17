de045e41fef30c68dfeb8f5f50be3675ee29730b
mohammedaathilk <mohammed.aathil@graytitude.com>
2024-04-17 16:25:37 +0530

    added

---
+++ bash.sh
@@ -0,0 +1,13 @@
+ Volume in drive C is Acer
+ Volume Serial Number is E096-89D5
+
+ Directory of C:\Users\aathil\Documents\proj
+
+17-04-2024  04:20 PM    <DIR>          .
+17-04-2024  04:00 PM    <DIR>          ..
+17-04-2024  04:20 PM                 0 bash.sh
+17-04-2024  03:54 PM               118 hello.java
+17-04-2024  04:14 PM             1,330 readme.md
+17-04-2024  04:04 PM               415 Simple.class
+               4 File(s)          1,863 bytes
+               2 Dir(s)  923,238,539,264 bytes free
